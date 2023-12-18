1676aea35137b2fb84f27af99ce8ace652bb41ce
Paolo224 <picariellopaolo224@gmail.com>
2023-12-18 10:59:54 +0100

    piccole modifiche alla dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -8,7 +8,7 @@
     <div class="row justify-content-center">
         <div class="col">
             <div class="card">
-                <div class="card-header">{{ __('User Dashboard') }}</div>
+                <div class="card-header">{{Auth::user()->cognome}} {{Auth::user()->nome}}</div>
 
                 <div class="card-body">
                     @if (session('status'))
