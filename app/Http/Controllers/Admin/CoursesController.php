bfe83432e4959ee468855457e0b81e0c660d2c60
Paolo224 <picariellopaolo224@gmail.com>
2023-12-19 12:16:27 +0100

    impostazione della index dei corsi + far si che solo i corsi delle mie agency si vedano

--- app/Http/Controllers/Admin/CoursesController.php
+++ app/Http/Controllers/Admin/CoursesController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Admin\Courses;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class CoursesController extends Controller
 {
@@ -13,9 +14,16 @@ class CoursesController extends Controller
      */
     public function index()
     {
-        $corsi = Courses::all();
+        // Ottieni l'utente autenticato (o l'utente di cui vuoi ottenere i corsi)
+        $user = auth()->user(); // Ottieni l'utente autenticato
 
-        return view('admin.course.index', compact('corsi'));
+        // Ottieni tutte le agenzie associate all'utente
+        $agencies = $user->agencies->pluck('id'); // Ottieni gli ID delle agenzie dell'utente
+
+        // Ottieni i corsi associati alle agenzie dell'utente
+        $courses = Courses::whereIn('agency_id', $agencies)->get();
+
+        return view('admin.course.index', compact('courses'));
     }
 
     /**
--- resources/views/admin/course/index.blade.php
+++ resources/views/admin/course/index.blade.php
@@ -1,5 +1,55 @@
 @extends('layouts.admin')
 
 @section('content')
-
+<div class="container-fluid container-xxl">
+    <div class="row mt-3">
+            <div class="col-12 text-end">
+                <a href="{{route('admin.course.create')}}" class="btn btn-sm btn-success" style="width: 80px;">
+                    + Nuovo
+                </a>
+            </div>
+            @if(!$courses->isNotEmpty())
+                <h3 class="text-center mt-5 mb-2">Non hai inserito nessuna Corso di Formazione...</h3>
+                <h4 class="text-center mb-2">Inserisci un corso cliccando il tasto "Nuovo"</h4>
+            @else
+            <table class="table table-striped table-hover mt-3 table-bordered">
+                <thead class="table-dark">
+                <tr>
+                    <th scope="col" class="text-center"></th>
+                    <th scope="col" class="text-center">Immagine</th>
+                    <th scope="col" class="text-center">Titolo</th>
+                    <th scope="col" class="text-center">Durata</th>
+                    <th scope="col" class="text-center">
+                        <a href="{{route('admin.course.create')}}" class="btn btn-sm btn-success">
+                            + Aggiungi un Azienda
+                        </a>
+                    </th>
+                </tr>
+                </thead>
+                <tbody>
+                    @foreach ($courses as $course)
+                        <tr>
+                            <td class="text-center align-middle">
+                                <img src="{{asset('storage/' . $course->agency->logo)}}" class="card-img-top me-2" alt="{{$course->titolo}}" style="width: 50px;">
+                            </td>
+                            <td class="text-center align-middle">
+                                <img src="{{asset('storage/' . $course->immagine)}}" class="card-img-top" alt="{{$course->titolo}}" style="width: 50px;">
+                            </td>
+                            <td class="text-center align-middle">{{$course->titolo}}</td>
+                            <td class="text-center align-middle">{{$course->durata}} ore</td>
+                            <td class="text-center align-middle">
+                                <a href="{{route('admin.course.show', $course->id)}}" class="btn btn-sm btn-primary">
+                                    Mostra
+                                </a>
+                                <a href="{{route('admin.course.edit', $course->id)}}" class="btn btn-sm btn-warning">
+                                    Modifica
+                                </a>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+            @endif
+        </div>
+    </div>
 @endsection
