ef299abb36f7749e002abdbbad828219dd0a45c3
Paolo224 <picariellopaolo224@gmail.com>
2024-01-09 17:11:14 +0100

    piccoli abbellimenti alla dashboard

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Admin\Agency;
+use App\Models\Admin\Course;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -11,7 +13,13 @@ class DashboardController extends Controller
     public function index()
     {
         $user = Auth::user();
+        $numberAgencies = $user->agencies->count();
 
-        return view('admin.dashboard', compact('user'));
+        $agencies = $user->agencies->pluck('id');
+        $courses = Course::whereIn('agency_id', $agencies)->get();
+        $numberCourses = $courses->count();
+
+
+        return view('admin.dashboard', compact('user', 'numberAgencies', 'numberCourses'));
     }
 }
